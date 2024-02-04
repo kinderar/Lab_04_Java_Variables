9b9646994362b1cf185951728bb5c69df8735c5d
kinderar <156221770+kinderar@users.noreply.github.com>
2024-02-04 15:32:52 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,46 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //declarations
+        int intOperandA = 1; //This is a declaration and initial assignment
+        int intOperandB = 5; //This is a declaration and initial assignment
+        int intSum = 13; //This is a declaration and initial assignment
+        int intProduct = 24; //This is a declaration and initial assignment
+        int intDifference = 14; //This is a declaration and initial assignment
+        int intQuotient = 25; //This is a declaration and initial assignment
+        int intModulo = 21; //This is a declaration and initial assignment
+        double doubleOperandA = 3.50; //This is a declaration and initial assignment
+        double doubleOperandB = 4.51; //This is a declaration and initial assignment
+        double doubleSum = 5.67; //This is a declaration and initial assignment
+        double doubleProduct = 6.42; //This is a declaration and initial assignment
+        double doubleDifference = 7.11; //This is a declaration and initial assignment
+        double doubleQuotient = 8.49; //This is a declaration and initial assignment
+            intSum = intOperandA + intOperandB; //Assignment that uses the arithmetic operator +
+            System.out.println("The sum using ints of " + intOperandA + " " + intOperandB + " is " + intSum); //Output statement
+            intProduct = intOperandA * intOperandB; //Assignment that uses the arithmetic operator *
+            System.out.println("The product using ints of " + intOperandA + " " + intOperandB + " is " + intProduct); //Output statement
+            intDifference = intOperandA - intOperandB; //Assignment that uses the arithmetic operator -
+            System.out.println("The difference using ints of " + intOperandA + " " + intOperandB + " is " + intDifference); //Output statement
+            intQuotient = intOperandA / intOperandB; //Assignment that uses the arithmetic operator /
+            System.out.println("The Quotient using ints of " + intOperandA + " " + intOperandB + " is " + intQuotient); //Output statement
+            intModulo = intOperandA % intOperandB; //Assignment that uses the arithmetic operator %
+            System.out.println("The modulo using ints of " + intOperandA + " " + intOperandB + " is " + intModulo); //Output statement
+            doubleSum = doubleOperandA + doubleOperandB; //Assignment that uses the arithmetic operator +
+            System.out.println("The sum using double of " + doubleOperandA + " " + doubleOperandB + " is " + doubleSum); //Output statement
+            doubleProduct = doubleOperandA * doubleOperandB; //Assignment that uses the arithmetic operator *
+            System.out.println("The product using double of " + doubleOperandA + " " + doubleOperandB + " is " + doubleProduct); //Output statement
+            doubleDifference = doubleOperandA - doubleOperandB; //Assignment that uses the arithmetic operator -
+            System.out.println("The difference using double of " + doubleOperandA + " " + doubleOperandB + " is " + doubleDifference); //Output statement
+            doubleQuotient = doubleOperandA / doubleOperandB; //Assignment that uses the arithmetic operator /
+            System.out.println("The quotient using double of " + doubleOperandA + " " + doubleOperandB + " is " + doubleQuotient); //Output statement
+        //declarations
+        int kidsInFamily = 2;
+        boolean isItRaining = true;
+        double gallonOfGas = 2.50;
+        double favoriteNumber = 15;
+        double shoeSize = 11.50;
+        int birthMonth = 9;
+        String fullName = "Alex Kinder";
+    }
+}
